2e9c309b603a60707d1ba8437cbc080b2d309af2
Samir <samss.benbella@gmail.com>
2021-09-01 09:21:59 +0200

    retour à la normal (problème des imports)

--- plant-front/src/app/app.module.ts
+++ plant-front/src/app/app.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import {NgModule} from "@angular/core";
 import {MonJardinComponent} from "./mon-jardin/mon-jardin.component";
 import {DejaPlanteComponent} from "./deja-plante/deja-plante.component";
 import {VegetalComponent} from "./vegetal/vegetal.component";
-import {RechercheAlternativeComponent} from "./recherche-alternative/recherche-alternative.component";
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
 import {RechercheComponent} from "./recherche/recherche.component";
 import {UtilisateurComponent} from "./utilisateur/utilisateur.component";
+import {CreationCompteComponent} from "./creation-compte/creation-compte.component";
 import {AccueilComponent} from "./accueil/accueil.component";
-import { CreationCompteComponent } from './creation-compte/creation-compte.component';
+import {RecetteComponent} from "./recette/recette.component";
+import {RechercheAlternativeComponent} from "./recherche-alternative/recherche-alternative.component";
+import {FormsModule} from "@angular/forms";
+import {AjoutRecetteComponent} from "./ajout-recette/ajout-recette.component";
 import {ConnexionComponent} from "./connexion/connexion.component";
-import { RecetteComponent } from './recette/recette.component';
-import { AjoutRecetteComponent } from './ajout-recette/ajout-recette.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -24,7 +23,6 @@ import { AjoutRecetteComponent } from './ajout-recette/ajout-recette.component';
     MonJardinComponent,
     DejaPlanteComponent,
     VegetalComponent,
-    PersonneComponent,
     RechercheComponent,
     RechercheAlternativeComponent,
     CreationCompteComponent,
