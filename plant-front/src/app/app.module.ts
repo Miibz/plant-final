1ff05c3c546a58999605b3a22825962fbb5c641b
Samir <samss.benbella@gmail.com>
2021-09-02 10:03:45 +0200

    Import fonctionel

--- plant-front/src/app/app.module.ts
+++ plant-front/src/app/app.module.ts
@@ -1,34 +1,29 @@
-import {NgModule} from "@angular/core";
-import {AppComponent} from "./app.component";
-import {MonJardinComponent} from "./mon-jardin/mon-jardin.component";
-import {DejaPlanteComponent} from "./deja-plante/deja-plante.component";
-import {UtilisateurComponent} from "./utilisateur/utilisateur.component";
-import {RechercheAlternativeComponent} from "./recherche-alternative/recherche-alternative.component";
-import {AccueilComponent} from "./accueil/accueil.component";
-import {RechercheComponent} from "./recherche/recherche.component";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
 import {VegetalComponent} from "./vegetal/vegetal.component";
-import {AdminAjoutPlanteComponent} from "./admin-ajout-plante/admin-ajout-plante.component";
+import {RechercheAlternativeComponent} from "./recherche-alternative/recherche-alternative.component";
 import {CreationCompteComponent} from "./creation-compte/creation-compte.component";
-import {FormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
+import {MonJardinComponent} from "./mon-jardin/mon-jardin.component";
+import {RechercheComponent} from "./recherche/recherche.component";
+import {DejaPlanteComponent} from "./deja-plante/deja-plante.component";
+import {AdminAjoutPlanteComponent} from "./admin/admin-ajout-plante/admin-ajout-plante.component";
+import {AjoutRecetteComponent} from "./ajout-recette/ajout-recette.component";
+import {AdminModifierPlanteComponent} from "./admin/admin-modifier-plante/admin-modifier-plante.component";
 import {LectureRecetteComponent} from "./lecture-recette/lecture-recette.component";
 import {AppRoutingModule} from "./app-routing.module";
-import {HttpClientModule} from "@angular/common/http";
-import {UtilisateurComponent} from "./utilisateur/utilisateur.component";
+import {LectureGuideComponent} from "./lecture-guide/lecture-guide.component";
 import {AccueilComponent} from "./accueil/accueil.component";
-import { AdminAjoutPlanteComponent } from './admin/admin-ajout-plante/admin-ajout-plante.component';
+import {UtilisateurComponent} from "./utilisateur/utilisateur.component";
 import {BarreDeRechercheComponent} from "./barre-de-recherche/barre-de-recherche.component";
-import { AjoutGuideComponent } from './ajout-guide/ajout-guide.component';
-import { LectureRecetteComponent } from './lecture-recette/lecture-recette.component';
-import { LectureGuideComponent } from './lecture-guide/lecture-guide.component';
-import { GuideComponent } from './guide/guide.component';
-import {RechercheComponent} from "./recherche/recherche.component";
-import {RechercheAlternativeComponent} from "./recherche-alternative/recherche-alternative.component";
-import {RecetteComponent} from "./recette/recette.component";
-import { AjoutRecetteComponent } from "./ajout-recette/ajout-recette.component";
 import {ConnexionComponent} from "./connexion/connexion.component";
-import {AdminModifierPlanteComponent} from "./admin/admin-modifier-plante/admin-modifier-plante.component";
+import {AjoutGuideComponent} from "./ajout-guide/ajout-guide.component";
 import {AffichageDetailPlanteComponent} from "./admin/affichage-detail-plante/affichage-detail-plante.component";
+import {GuideComponent} from "./guide/guide.component";
+import {RecetteComponent} from "./recette/recette.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
 
 
 @NgModule({
@@ -43,7 +38,6 @@ import {AffichageDetailPlanteComponent} from "./admin/affichage-detail-plante/af
     AccueilComponent,
     AdminAjoutPlanteComponent,
     CreationCompteComponent,
-    UtilisateurComponent,
     AccueilComponent,
     RecetteComponent,
     BarreDeRechercheComponent,
